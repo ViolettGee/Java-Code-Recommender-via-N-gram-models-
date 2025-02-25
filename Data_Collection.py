22d27604fa99c77f1ea405bedd11279bf3cf7554
Ella Bryant <ellagb7@gmail.com>
2025-02-25 17:48:55 -0500

    adjusted function ordering in Data_collectiongit: 'committ' is not a git command. See 'git --help'.
    The most similar command is
    	commit
    [?2004h[?1049h[22;0;0t[1;24r(B[m[4l[?7h[39;49m[?1h=[?1h=[?25l[39;49m(B[m[H[2J[22d(B[0;7mFile _ is being edited by egbryant01 (with nano 6.2, PID 2537866); open anyway? [23;1H Y(B[m Yes
    [24d(B[0;7m N(B[m No[24;17H(B[0;7m^C(B[m Cancel[22;80H[?12l[?25h[?25l[?12l[?25h[?25l[?12l[?25h[?25l
    [J[24d[?12l[?25h[24;1H[?1049l[23;0;0t
    [?1l>[?2004lAborting commit due to empty commit message.

--- Data_Collection.py
+++ Data_Collection.py
@@ -3,7 +3,7 @@ import requests
 import pandas as pd
 
 #function that retrieves url info for each of the repositories based on a csv file row
-def parse_row_info(row):
+def parse_row_info (row):
   #repository owner name and repository name initialization
   repo_owner = row[0]
   repo = row[1]
@@ -14,12 +14,27 @@ def parse_row_info(row):
   for i in range(len(row) - 2):
     
     #check object is a string type
-    if type(row[i+2] == type('str'):
+    if type(row[i+2]) == type('str'):
       #add object content to current string path
       path = path + '/' + row[i+2]
 
   #return relevant data about repository
   return(repo_owner, repo, path)
+
+#function that creates a text file with the data collected
+def java_to_text (repo_owner, repo, file_name, file_path):
+  #initialize url
+  url = f"https://raw.githubusercontent.com/{repo_owner}/{repo}/master/{file_path}"
+
+  #retrieve response with request module
+  response = requests.get(url)
+
+  #check success of request
+  if response.status_code == 200:
+
+    #write file text to new file in current repo
+    with open(f"RawData/{repo}{file_name}.txt", 'w') as file:
+      file.write(response.text)
   
 #function that retrieves all the types of a specific file in a section of a repository
 def get_files (repo_owner, repo, path, file_extension):
@@ -40,21 +55,6 @@ def get_files (repo_owner, repo, path, file_extension):
       if file['name'].endswith(file_extension):
         #call file write function
         java_to_txt(repo_owner, repo, file['name'], file['path'])
-  
-#function that creates a text file with the data collected
-def java_to_text(repo_owner, repo, file_name, file_path):
-  #initialize url
-  url = f"https://raw.githubusercontent.com/{repo_owner}/{repo}/master/{file_path}"
-  
-  #retrieve response with request module
-  response = requests.get(url)
-  
-  #check success of request
-  if response.status_code == 200:
-
-    #write file text to new file in current repo
-    with open(f"RawData/{repo}{file_name}.txt", 'w') as file:
-      file.write(response.text)
   
 #main section running through the csv file with repository data
 #initialize data frame from csv file
