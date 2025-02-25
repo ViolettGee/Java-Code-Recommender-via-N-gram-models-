7cc1ac1cda52cafdd5528bd08170bae85bcddb11
ViolettGee <157923420+ViolettGee@users.noreply.github.com>
2025-02-25 03:58:28 -0500

    Update Data_Collection.py

--- Data_Collection.py
+++ Data_Collection.py
@@ -1,1 +1,9 @@
+#import necessary libraries
 
+#function that retrieves url info for each of the repositories based on a csv file row
+
+#function that retrieves all the types of a specific file in a section of a repository
+
+#function that creates a text file with the data collected
+
+#main section running through the csv file with repository data
