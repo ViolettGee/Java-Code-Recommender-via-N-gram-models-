11be9e68c826ad3928a2b5b47e62545405bbec20
ViolettGee <157923420+ViolettGee@users.noreply.github.com>
2025-02-25 04:01:04 -0500

    Update Data_Collection.py

--- Data_Collection.py
+++ Data_Collection.py
@@ -1,9 +1,17 @@
 #import necessary libraries
+import requests
+import pandas as pd
 
 #function that retrieves url info for each of the repositories based on a csv file row
-
+def parse_row_info(row):
+  return #placeholder
+  
 #function that retrieves all the types of a specific file in a section of a repository
-
+def get_files (repo_owner, repo, path, file_extension):
+  return #placeholder
+  
 #function that creates a text file with the data collected
-
+def java_to_text(repo_owner, repo, file_name, file_path):
+  return #placeholder
+  
 #main section running through the csv file with repository data
