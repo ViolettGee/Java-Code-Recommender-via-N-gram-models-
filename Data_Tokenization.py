27f4cc1ad409f84de25756cd3d9ebc7492a6b35f
Ella Bryant <egbryant01@wm.ed>
2025-03-04 21:10:17 -0500

    preplanning for Data_Tokenization file

--- Data_Tokenization.py
+++ Data_Tokenization.py
@@ -1,1 +1,7 @@
+#initialize the necessary imports
 
+#function that tokenizes methods
+
+#function that writes to an output csv the tokenization data
+
+#main code that reads the preprocessed data and calls the above methods to tokenize the data
