b9bf500d70ed85dab871f5d9685444220cad0379
Ella Bryant <egbryant01@wm.ed>
2025-03-04 21:16:18 -0500

    preplanning for writing rows to the output file

--- Data_Tokenization.py
+++ Data_Tokenization.py
@@ -1,4 +1,6 @@
 #initialize the necessary imports
+import javalang
+import csv
 
 #function that tokenizes methods
 def method_tokenization(text):
@@ -10,5 +12,15 @@ def method_tokenization(text):
     return(tokens)
 
 #function that writes to an output csv the tokenization data
+
+    #open the output file for the tokenization data
+
+        #initialize the writer object for the file using the csv module
+
+        #iterate through the methods
+
+            #initialize the row of using that particular method's name and tokenized text
+
+            #write to the file using the writer object
 
 #main code that reads the preprocessed data and calls the above methods to tokenize the data
