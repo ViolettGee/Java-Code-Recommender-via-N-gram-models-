3eed613dc29e8035e6ac96edf954f49371e964d6
Ella Bryant <egbryant01@wm.ed>
2025-03-04 21:11:48 -0500

    preplanning for tokenization function

--- Data_Tokenization.py
+++ Data_Tokenization.py
@@ -2,6 +2,10 @@
 
 #function that tokenizes methods
 
+    #create tokens using javalang library
+
+    #return tokens in list format
+
 #function that writes to an output csv the tokenization data
 
 #main code that reads the preprocessed data and calls the above methods to tokenize the data
