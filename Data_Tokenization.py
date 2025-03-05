83cbef45dba034b10ee598a894ace4b9b4f6e309
Ella Bryant <egbryant01@wm.ed>
2025-03-04 21:13:11 -0500

    implement method tokenizing function

--- Data_Tokenization.py
+++ Data_Tokenization.py
@@ -1,10 +1,13 @@
 #initialize the necessary imports
 
 #function that tokenizes methods
-
+def method_tokenization(text):
+    
     #create tokens using javalang library
-
+    tokens = list(javalang.tokenizer.tokenize(text))
+    
     #return tokens in list format
+    return(tokens)
 
 #function that writes to an output csv the tokenization data
 
